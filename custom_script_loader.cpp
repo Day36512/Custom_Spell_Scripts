f9310c1917936f3e1fa11fc4d865043440ec054c
Dinkledork <118951051+Day36512@users.noreply.github.com>
2023-09-28 03:24:30 -0600

    Update custom_script_loader.cpp

--- custom_script_loader.cpp
+++ custom_script_loader.cpp
@@ -15,43 +15,15 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-void AddSC_REFORGER_NPC();
-void AddSC_TwoForms();
-void AddSC_LoginSpells();
-void AddSC_AttackerSpawnModule();
-void AddSC_KindlingFuryHandler();
 void AddSC_Xaveric();
-void AddSC_Hardcore();
-void AddSC_SafeAreas();
-void AddSC_TENMAN();
-void AddSC_spell_custom_haste_to_speed();
-void AddSC_KarazhanTW();
-void AddSC_Ghoul_Dust_Trinket();
-void AddSC_crazy_cat_lady_and_cat();
 void AddSC_spell_mage_scorch_execute();
 
 
 
-//void AddSC_boss_the_hidden_watcher();
-//void AddSC_SymbiosisScript();
+
 
 void AddCustomScripts()
 {
-    AddSC_REFORGER_NPC();
-    AddSC_TwoForms();
-    AddSC_LoginSpells();
-    AddSC_AttackerSpawnModule();
-    AddSC_KindlingFuryHandler();
     AddSC_Xaveric();
-    AddSC_Hardcore();
-    AddSC_SafeAreas();
-    AddSC_TENMAN();
-    AddSC_spell_custom_haste_to_speed();
-    AddSC_KarazhanTW();
-    AddSC_Ghoul_Dust_Trinket();
-    AddSC_crazy_cat_lady_and_cat();
     AddSC_spell_mage_scorch_execute();
-
-   // AddSC_boss_the_hidden_watcher();
-    // AddSC_SymbiosisScript();
 }
