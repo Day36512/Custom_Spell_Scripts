037477c5501c387529e37ba6ce1ea2375d99e465
Dinkledork <118951051+Day36512@users.noreply.github.com>
2023-09-28 03:23:39 -0600

    Add files via upload

---
+++ custom_script_loader.cpp
@@ -0,0 +1,57 @@
+/*
+ * This file is part of the AzerothCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+void AddSC_REFORGER_NPC();
+void AddSC_TwoForms();
+void AddSC_LoginSpells();
+void AddSC_AttackerSpawnModule();
+void AddSC_KindlingFuryHandler();
+void AddSC_Xaveric();
+void AddSC_Hardcore();
+void AddSC_SafeAreas();
+void AddSC_TENMAN();
+void AddSC_spell_custom_haste_to_speed();
+void AddSC_KarazhanTW();
+void AddSC_Ghoul_Dust_Trinket();
+void AddSC_crazy_cat_lady_and_cat();
+void AddSC_spell_mage_scorch_execute();
+
+
+
+//void AddSC_boss_the_hidden_watcher();
+//void AddSC_SymbiosisScript();
+
+void AddCustomScripts()
+{
+    AddSC_REFORGER_NPC();
+    AddSC_TwoForms();
+    AddSC_LoginSpells();
+    AddSC_AttackerSpawnModule();
+    AddSC_KindlingFuryHandler();
+    AddSC_Xaveric();
+    AddSC_Hardcore();
+    AddSC_SafeAreas();
+    AddSC_TENMAN();
+    AddSC_spell_custom_haste_to_speed();
+    AddSC_KarazhanTW();
+    AddSC_Ghoul_Dust_Trinket();
+    AddSC_crazy_cat_lady_and_cat();
+    AddSC_spell_mage_scorch_execute();
+
+   // AddSC_boss_the_hidden_watcher();
+    // AddSC_SymbiosisScript();
+}
